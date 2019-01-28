3e19726b735c7c15448321435e2affc15095e9e3
holdthebeach <holdthebeach@gmail.com>
2019-01-28 18:47:13 +0100

    added functionality for pushing roman numeral values for tens and ones place into array

--- scripts.js
+++ scripts.js
@@ -23,32 +23,32 @@ function ready() {
     var fifty = 'L'
     var hundred = 'C';
     var fiveHundred = 'D';
-    var oneThousand = 'M';
+    var thousand = 'M';
 
     var outputRomanNumerals = [];
     var outputValue = '';
 
     function convertHundredsInteger (hundredsValue) {
       var hundredsMultiple = '';
-      if(hundredsValue <= 3) {
+      if (hundredsValue <= 3) {
         hundredsMultiple = hundred.repeat(hundredsValue);
         console.log(hundredsMultiple);
         outputRomanNumerals.push(hundredsMultiple);
         console.log(outputRomanNumerals);
-      }else if(hundredsValue == 4) {
-        hundredsMultiple = 'CD';
+      }else if (hundredsValue == 4) {
+        hundredsMultiple = hundred + fiveHundred;
         outputRomanNumerals.push(hundredsMultiple);
         console.log(outputRomanNumerals);
       }else if (hundredsValue == 5) {
-        hundredsMultiple = 'D';
+        hundredsMultiple = fiveHundred;
         outputRomanNumerals.push(hundredsMultiple);
         console.log(outputRomanNumerals);
       }else if (hundredsValue <= 8) {
-        hundredsMultiple = 'D' + hundred.repeat(hundredsValue-5);
+        hundredsMultiple = fiveHundred + hundred.repeat(hundredsValue-5);
         outputRomanNumerals.push(hundredsMultiple);
         console.log(outputRomanNumerals);
-      }else{
-        hundredsMultiple = 'CM';
+      }else {
+        hundredsMultiple = hundred + thousand;
         outputRomanNumerals.push(hundredsMultiple);
         console.log(outputRomanNumerals);
       }
@@ -56,12 +56,62 @@ function ready() {
     convertHundredsInteger(inputFirstInteger);
 
     function convertTensInteger (tensValue) {
-
+      var tensMultiple = '';
+      if (tensValue == 0){
+        return '';
+      }else if (tensValue <= 3) {
+        tensMultiple = ten.repeat(tensValue);
+        console.log(tensMultiple);
+        outputRomanNumerals.push(tensMultiple);
+        console.log(outputRomanNumerals);
+      }else if (tensValue == 4) {
+        tensMultiple = ten + fifty;
+        outputRomanNumerals.push(tensMultiple);
+        console.log(outputRomanNumerals);
+      }else if (tensValue == 5) {
+        tensMultiple = fifty;
+        outputRomanNumerals.push(tensMultiple);
+        console.log(outputRomanNumerals);
+      }else if (tensValue <= 8) {
+        tensMultiple = fifty + ten.repeat(tensValue-5);
+        outputRomanNumerals.push(tensMultiple);
+        console.log(outputRomanNumerals);
+      }else {
+        tensMultiple = ten + hundred;
+        outputRomanNumerals.push(tensMultiple);
+        console.log(outputRomanNumerals);
+      }
     }
+    convertTensInteger(inputSecondInteger);
 
     function convertOnesInteger (onesValue) {
-
+      var onesMultiple = '';
+      if (onesValue == 0){
+        return '';
+      }else if (onesValue <= 3) {
+        onesMultiple = one.repeat(onesValue);
+        console.log(onesMultiple);
+        outputRomanNumerals.push(onesMultiple);
+        console.log(outputRomanNumerals);
+      }else if (onesValue == 4) {
+        onesMultiple = one + five;
+        outputRomanNumerals.push(onesMultiple);
+        console.log(outputRomanNumerals);
+      }else if (onesValue == 5) {
+        onesMultiple = five;
+        outputRomanNumerals.push(onesMultiple);
+        console.log(outputRomanNumerals);
+      }else if (onesValue <= 8) {
+        onesMultiple = five + one.repeat(onesValue-5);
+        outputRomanNumerals.push(onesMultiple);
+        console.log(outputRomanNumerals);
+      }else {
+        onesMultiple = one + ten;
+        outputRomanNumerals.push(onesMultiple);
+        console.log(outputRomanNumerals);
+      }
     }
+    convertOnesInteger(inputThirdInteger);
 
     function convertEntireValue () {
       outputValue = convertHundredsInteger(inputFirstInteger) +
