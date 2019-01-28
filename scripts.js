9c0c9aee3b1b10aa2c8d3bda69291cd840b92c7d
holdthebeach <holdthebeach@gmail.com>
2019-01-28 19:41:40 +0100

    combined the three integer converting functions into one by adding more parameters and added a function to call it and concatenate the roman numerals and set to the value for #output

--- scripts.js
+++ scripts.js
@@ -4,9 +4,7 @@ function ready() {
   var form = document.getElementById('numberConverter');
   var input = document.getElementById('arabicNumber');
   var output = document.getElementById('romanNumber');
-  var outputValue = 0;
 
-  console.log(form);
   form.onsubmit = submit;
   function submit(){
     var inputValue = input.value;
@@ -28,101 +26,37 @@ function ready() {
     var outputRomanNumerals = [];
     var outputValue = '';
 
-    function convertHundredsInteger (hundredsValue) {
-      var hundredsMultiple = '';
-      if (hundredsValue <= 3) {
-        hundredsMultiple = hundred.repeat(hundredsValue);
-        console.log(hundredsMultiple);
-        outputRomanNumerals.push(hundredsMultiple);
-        console.log(outputRomanNumerals);
-      }else if (hundredsValue == 4) {
-        hundredsMultiple = hundred + fiveHundred;
-        outputRomanNumerals.push(hundredsMultiple);
-        console.log(outputRomanNumerals);
-      }else if (hundredsValue == 5) {
-        hundredsMultiple = fiveHundred;
-        outputRomanNumerals.push(hundredsMultiple);
-        console.log(outputRomanNumerals);
-      }else if (hundredsValue <= 8) {
-        hundredsMultiple = fiveHundred + hundred.repeat(hundredsValue-5);
-        outputRomanNumerals.push(hundredsMultiple);
-        console.log(outputRomanNumerals);
+    function convertInteger (integer, upTwoLevels, upOneLevel, base) {
+      var multiple = '';
+      if (integer <= 3) {
+        multiple = base.repeat(integer);
+        outputRomanNumerals.push(multiple);
+      }else if (integer == 4) {
+        multiple = base + upOneLevel;
+        outputRomanNumerals.push(multiple);
+      }else if (integer == 5) {
+        multiple = upOneLevel;
+        outputRomanNumerals.push(multiple);
+      }else if (integer <= 8) {
+        multiple = upOneLevel + base.repeat(integer-5);
+        outputRomanNumerals.push(multiple);
       }else {
-        hundredsMultiple = hundred + thousand;
-        outputRomanNumerals.push(hundredsMultiple);
-        console.log(outputRomanNumerals);
+        multiple = hundred + upTwoLevels;
+        outputRomanNumerals.push(multiple);
       }
     }
-    convertHundredsInteger(inputFirstInteger);
-
-    function convertTensInteger (tensValue) {
-      var tensMultiple = '';
-      if (tensValue == 0){
-        return '';
-      }else if (tensValue <= 3) {
-        tensMultiple = ten.repeat(tensValue);
-        console.log(tensMultiple);
-        outputRomanNumerals.push(tensMultiple);
-        console.log(outputRomanNumerals);
-      }else if (tensValue == 4) {
-        tensMultiple = ten + fifty;
-        outputRomanNumerals.push(tensMultiple);
-        console.log(outputRomanNumerals);
-      }else if (tensValue == 5) {
-        tensMultiple = fifty;
-        outputRomanNumerals.push(tensMultiple);
-        console.log(outputRomanNumerals);
-      }else if (tensValue <= 8) {
-        tensMultiple = fifty + ten.repeat(tensValue-5);
-        outputRomanNumerals.push(tensMultiple);
-        console.log(outputRomanNumerals);
-      }else {
-        tensMultiple = ten + hundred;
-        outputRomanNumerals.push(tensMultiple);
-        console.log(outputRomanNumerals);
-      }
-    }
-    convertTensInteger(inputSecondInteger);
-
-    function convertOnesInteger (onesValue) {
-      var onesMultiple = '';
-      if (onesValue == 0){
-        return '';
-      }else if (onesValue <= 3) {
-        onesMultiple = one.repeat(onesValue);
-        console.log(onesMultiple);
-        outputRomanNumerals.push(onesMultiple);
-        console.log(outputRomanNumerals);
-      }else if (onesValue == 4) {
-        onesMultiple = one + five;
-        outputRomanNumerals.push(onesMultiple);
-        console.log(outputRomanNumerals);
-      }else if (onesValue == 5) {
-        onesMultiple = five;
-        outputRomanNumerals.push(onesMultiple);
-        console.log(outputRomanNumerals);
-      }else if (onesValue <= 8) {
-        onesMultiple = five + one.repeat(onesValue-5);
-        outputRomanNumerals.push(onesMultiple);
-        console.log(outputRomanNumerals);
-      }else {
-        onesMultiple = one + ten;
-        outputRomanNumerals.push(onesMultiple);
-        console.log(outputRomanNumerals);
-      }
-    }
-    convertOnesInteger(inputThirdInteger);
 
     function convertEntireValue () {
-      outputValue = convertHundredsInteger(inputFirstInteger) +
-        convertTensInteger (inputSecondInteger) +
-        convertOnesInteger (inputThirdInteger);
+      convertInteger(inputFirstInteger, thousand, fiveHundred, hundred);
+      convertInteger(inputSecondInteger, hundred, fifty, ten);
+      convertInteger(inputThirdInteger, ten, five, one);
+      outputRomanNumerals.forEach(function(value) {
+        outputValue = outputValue + value;
+      })
+      output.innerHTML = outputValue;
     }
 
-    console.log(inputFirstInteger);
-    console.log(inputSecondInteger);
-    console.log(inputThirdInteger);
-    output.innerHTML = inputFirstChar + inputSecondChar + inputThirdChar;
+    convertEntireValue();
     event.preventDefault();
     form.reset();
   }
